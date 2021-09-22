82c5244a08795cda659849dac428aed0fd608743
ChristosHadjigeorghiou1996 <christoshadjigeorghiou@gmail.com>
2021-09-22 13:55:58 +0200

    fixed nexgenpop, check running twice per nextgenpop

--- dummy_demo.py
+++ dummy_demo.py
@@ -6,6 +6,8 @@
 
 # imports framework
 import sys, os
+
+from numpy.core.fromnumeric import shape
 sys.path.insert(0, 'evoman') 
 from demo_controller import player_controller
 from environment import Environment
@@ -60,9 +62,9 @@ elif experiment_type == "":
 # size of individuals
 individuals_size = (env.get_num_sensors()+1)*hidden_neurons + (hidden_neurons+1)*5
 # population size made of n individuals
-population_size = 100
+population_size = 10
 # max generations to run
-generations = 20
+generations = 5
 # max iterations to run without improvement to indicate stagnation
 improvement_value = 0
 improvement_counter = 0 # cap it to 15
@@ -93,25 +95,34 @@ crossover_alpha_parameter= 0.4
 ## simple genetics
 def create_random_uniform_population(size_of_populations, size_of_individuals):
     new_population = np.random.uniform(lower_limit_uniform_population, upper_limit_uniform_population, (size_of_populations, size_of_individuals))
-    # print(f'new population.shape: {new_population.shape}')
+    # print(f'new_population: {new_population} and new population.shape: {new_population.shape}')
+
     return new_population
 
 def test_individual(individual, env):
     individual_finess, individual_player_life, individual_enemy_life, individual_run_time = env.play(pcont=individual)
     # apparently creating numpy array from list of lists is depreciated - needs dtype=object
-    individual_info = np.array([individual, individual_finess, individual_player_life, individual_enemy_life, individual_run_time], dtype=object)
-    return individual_info
+    
+    individual_info = np.array((individual_finess, individual_player_life, individual_enemy_life, individual_run_time))
+    print(f'individual_info: {individual_info} and shape: {individual_info.shape}')
+    return individual, individual_info
 
 def find_best_individual(population, env):
-    # should be 0 but fitness is negative.. 
-    best_individual_fitness = -10 
+    best_individual_fitness = -10
+    best_individual=population[0]
+    best_individual, best_individual_information= test_individual(best_individual, env) 
+
     for individual in population:
-        # print(f'individual:\n{individual}')
-        individual_information= test_individual(individual, env)
-        if best_individual_fitness < individual_information[1]:
-            best_individual_fitness = individual_information[1]
-            best_individual = individual_information
-    return individual_information
+        current_individual, individual_information= test_individual(individual, env)
+        # print(f'individual_information[0]: {individual_information[0]} and type: {type(individual_information[0])}'    )
+        if best_individual_fitness < individual_information[0]:
+            # print(f'new_best: {individual_information}')
+            best_individual_fitness= individual_information[0]
+            best_individual = current_individual
+            best_individual_information = individual_information
+    # print(f'best_individual: {best_individual}')
+    # print(f'best_individual_information: {best_individual_information}')
+    return best_individual, best_individual_information
 
 
 # randomly select two parents in the population and combine to produce two children with param 0.4 
@@ -122,13 +133,13 @@ def crossover_two_parents_two_children(first_parent, second_parent, alpha_parame
     # print(f'P1: {first_parent}, P2: {second_parent}')
     first_offspring = first_parent*alpha_parameter + (1-alpha_parameter) * second_parent 
     # print(f'first_offspring: {first_offspring}')
-    first_offspring_information = test_individual(first_offspring, env)
+    first_offspring_values, first_offspring_information = test_individual(first_offspring, env)
     # print(f'first_offspring_information: {first_offspring_information}')
     second_offspring = second_parent * alpha_parameter + ( 1 - alpha_parameter) * first_parent
     # print(f'second_offspring: {second_offspring}')
-    second_offspring_information = test_individual(second_offspring, env)
+    second_offspring_values, second_offspring_information = test_individual(second_offspring, env)
     # print(f'second_offspring_information: {second_offspring_information}')
-    return first_offspring_information, second_offspring_information
+    return first_offspring_values, first_offspring_information, second_offspring_values, second_offspring_information
 
 # def blend_crossover
 #     # abs distance between parents 
@@ -136,7 +147,7 @@ def crossover_two_parents_two_children(first_parent, second_parent, alpha_parame
 
 # randomly select two parents and ensure they are not the same from given population
 def randomly_select_two_individuals(provided_population):
-    print(f'provided_population.shape: {provided_population.shape}')
+    # print(f'provided_population.shape: {provided_population.shape}')
     parent_one_position = np.random.randint(0, provided_population.shape[0])
     parent_one= provided_population[parent_one_position]
     # print(f'parent_one_position: {parent_one_position} which is: \n {parent_one}')
@@ -153,45 +164,43 @@ def randomly_select_two_individuals(provided_population):
     # mask second parent
     mask_chosen_value[parent_two_position] = False
     remaining_population= provided_population[mask_chosen_value]
-    print(f'remaining_population.shape: {remaining_population.shape}')
+    # print(f'remaining_population.shape: {remaining_population.shape}')
 
     return parent_one, parent_two, remaining_population
 
 def mutate_individual(individual):
     random_probability = np.random.random() 
-    print(f'random_probability: {random_probability}')
+    # print(f'random_probability: {random_probability}')
     # if prob > 0.25 
     if random_probability > mutation_probability:
-        print('mutate individual') 
+        # print('mutate individual') 
         # mutate by considering some noise drawn from Gaussian distribution and then check if the random probability is > 0.5 in which case add otherwise subtract 
         if random_probability > 0.5: 
             mutated_individual = individual + np.random.normal(0, 1, individual.shape)
         else:
             mutated_individual = individual - np.random.normal(0, 1, individual.shape)
-        print(f'mutated individual: {mutated_individual}')
+        # print(f'mutated individual: {mutated_individual}')
         return mutated_individual
     return individual
         # maybe consider uniform from -1 to 1 instead 
     
 def create_new_population(old_population, environment):
-    print(f'old_population.shape: {old_population.shape} and values: {old_population}')
-    next_generation_population=np.zeros(old_population.shape)
-    print(f'next_generation_population.shape: {next_generation_population.shape} and values: {next_generation_population}')
-    print(f'type of old_population: {type(old_population)} and next_gen: {type(next_generation_population)}')
+    # print(f'old_population.shape: {old_population.shape} and values: {old_population}')
+    next_generation_population=np.zeros_like(old_population)
+    # print(f'next_generation_population.shape: {next_generation_population.shape} and values: {next_generation_population}')
+    # print(f'type of old_population: {type(old_population)} and next_gen: {type(next_generation_population)}')
     # go two steps each time removing two parents and creating two offsprings which are added to the new population. Stop until we are done whichi s of same size.
     for individual_position in range(0, old_population.shape[0], 2):
         # select two parents from the population ensuring they are not the same and forgetting them from the population which assumes they reproduce once 
         parent_one, parent_two, old_population= randomly_select_two_individuals(old_population)
         # crossover with arithmetic average with crossover_two_parents_two_childrenalpha 0.4 
-        offspring_one_information, offspring_two_information = crossover_two_parents_two_children(parent_one, parent_two, crossover_alpha_parameter, environment)
+        offspring_one, offspring_one_information, offspring_two, offspring_two_information = crossover_two_parents_two_children(parent_one, parent_two, crossover_alpha_parameter, environment)
         # undergo process of mutation
-        print(f'offspring_one_information[0]: {offspring_one_information[0]}')
-        modified_offspring_one =mutate_individual(offspring_one_information[0])
-        next_generation_population[individual_position][1]= modified_offspring_one
-        modified_offspring_two = mutate_individual(offspring_two_information[0])
-        next_generation_population[individual_position + 1][1] = modified_offspring_two[0]
-
-    print('next_generation_population.shape: {next_generation_population.shape} and values: {next_generation_population}')
+        modified_offspring_one =mutate_individual(offspring_one)
+        # print(f'modified_offspring_one type: {type(modified_offspring_one)} and shape: {modified_offspring_one.shape}')
+        modified_offspring_two = mutate_individual(offspring_two)
+        next_generation_population[individual_position]= modified_offspring_one
+        next_generation_population[individual_position + 1]= modified_offspring_two
 
     return next_generation_population
 
@@ -203,23 +212,37 @@ if __name__ == '__main__':
         env.update_parameter('enemies', [enemy])
         # log state of the env
         # env.state_to_log()
-        for new_generations in range(generations):
-            print(f'***Starting generation {new_generations}/{generations-1}')
+        # create an np.array with the size of generations 
+
+        for new_generation in range(generations):
+            print(f'***Starting generation {new_generation}/{generations-1}')
             if maximum_improvement_counter != improvement_counter:
-                random_uniform_population = create_random_uniform_population(population_size, individuals_size)
-                best_individual = find_best_individual(random_uniform_population, env)
-                print(f'best_individual: {best_individual}')
-                new_population = create_new_population(random_uniform_population, env)
-
+                if new_generation == 0:
+                    # record includes 4 values: fitness, player_life, enemy_life and time 
+                    best_individuals_array = np.zeros(shape=(generations, 4))
+                    print(f'best_individuals_array:\n{best_individuals_array}\n and shape: {best_individuals_array.shape}')
+                    generation_population = create_random_uniform_population(population_size, individuals_size)
+                    best_individual, best_individual_information = find_best_individual(generation_population, env)
+                    best_individuals_array[new_generation] = best_individual_information
+                    print(f'best_individuals_array:\n{best_individuals_array}')
+                    continue
+                print('best_individuals_array:\n{best_individuals_array}')
+                generation_population = create_new_population(generation_population, env)
+                print(f'generation_population.shape:{generation_population.shape}')
+                best_individual, best_individual_information = find_best_individual(generation_population, env)
+                best_individuals_array[new_generation] = best_individual_information
                 break
-                # if improvement value stays the same, increment the counter as there is no improvement. Ensure that there is improvement with mutation & crossover
-                if improvement_value == best_individual[1]:
-                    improvement_counter = improvement_counter + 1
-                    if improvement_counter == maximum_improvement_counter:
-                        break
-                else:
-                    improvement_value= best_individual[1]
-                    improvement_counter = 0
+        print(f'best_individuals_array:\n{best_individuals_array}')
 
-                break
+                # break
+                # # if improvement value stays the same, increment the counter as there is no improvement. Ensure that there is improvement with mutation & crossover
+                # if improvement_value == best_individual[1]:
+                #     improvement_counter = improvement_counter + 1
+                #     if improvement_counter == maximum_improvement_counter:
+                #         break
+                # else:
+                #     improvement_value= best_individual[1]
+                #     improvement_counter = 0
+
+                # break
 
